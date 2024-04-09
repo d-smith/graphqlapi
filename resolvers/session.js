dcdb9c54c7d51b60374bbe6d9d6a78fa97ea69c1
d-smith <doug.smith.mail@gmail.com>
2024-04-09 09:39:19 -0700

    refactored resolvers

---
+++ resolvers/session.js
@@ -0,0 +1,17 @@
+const _ = require('lodash');
+
+module.exports =  {
+    // Could loop over the speakers and retrieve details from
+    // each one instead of grabbing all speakers and filtering out
+    // everyone that is not a speaker for this session. This
+    // implementation takes advantage of the speaker list not being
+    // that large and the caching done by Apollo Server.
+    async speakers(session, args, { dataSources }, info) {
+        const speakers = await dataSources.speakerAPI.getSpeakers();
+        const filtered = speakers.filter(speaker => {
+            return _.filter(session.speakers, { id: speaker.id }).length > 0;
+        });
+        console.log(filtered);
+        return filtered;
+    }
+};
